fc629b9243e84969f46c7508dbfe9cecfe90ede4
Gabriel Viana <123520796+gabrielvianaa@users.noreply.github.com>
2024-02-27 21:22:51 -0300

    Add files via upload
    Modulo sobre vetor e laços em JS

---
+++ vetorlacos/vetorlacos.js
@@ -0,0 +1,41 @@
+
+// let notas = [5, 4, 3, 5, 2];
+// let soma = 0;
+// for(let i = 0; i < 5; i++){
+//     let nota = notas[i];
+//     soma += nota;
+
+// }
+
+// const media = soma / notas.length;
+
+// console.log(media.toFixed(2));
+
+//Crie um programa que dado um número imprima a sua tabuada.
+
+// let n1 = 4, i = 1, result = 0;
+
+// for( i; i <= 10; i++ ){
+//     result = n1 * i;
+//     console.log(n1, 'x', i, '=', result);
+// }
+
+/* 2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número par encontrado.*/
+
+// let i = 1;
+
+// for (i; i <= 10; i++){
+//     if( i%2 == 0 ){
+//         console.log('Esse número é par: ', i);
+//     }
+// }
+
+// //Usando vetor no mesmo exercício. 
+
+// let n1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// for (let i = 0; i <= n1.length; i++){
+//     if(n1%2 === 0){
+//         console.log('Esse número é par: ', n1);
+//     }
+// }
